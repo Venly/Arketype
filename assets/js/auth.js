273fc8b1dd55434112552a5274f63f146b3f7f95
Steve Maris <steve_maris@hotmail.com>
2019-05-17 15:53:44 +0200

    ARKANE-776 ARKANE-783

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -2,18 +2,7 @@
     'use strict';
 
     window.app = window.app || {};
-
-    app.env = 'tst1';
-    app.clientId = 'Arketype';
-    app.isLocal = true;
-    app.environment = {
-        env: app.env,
-        connect: app.env + (app.isLocal ? '-local' : ''),
-        api: 'https://api-' + app.env + '.arkane.network',
-        login: 'https://login-' + app.env + '.arkane.network',
-        arketypeClientSecret: '02053a9d-8293-43c4-a201-f8669f1329af', // Only visible for demo purpose, this secret should be configured in application backend
-    };
-
+    
     app.initApp = function() {
         window.arkaneConnect = new ArkaneConnect(app.clientId, {environment: app.environment.connect, windowMode: 'REDIRECT'});
         app.handleWindowModeTypeSwitch();
