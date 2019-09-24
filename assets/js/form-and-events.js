6394bf5e550fc2bb10de0ff9db2fcbda5cc6739c
davyvanroy <davy.van.roy@gmail.com>
2019-09-24 09:24:54 +0200

    use approve as function for contract calls (this always works)

--- assets/js/form-and-events.js
+++ assets/js/form-and-events.js
@@ -183,8 +183,8 @@
             walletId: {type: 'wallet-select', label: 'From'},
             to: {type: 'input', label: 'Contract Address', defaultValue: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2'},
             value: {type: 'input', label: 'Amount (in WEI)', defaultValue: '0'},
-            functionName: {type: 'input', label: 'Function Name', defaultValue: 'transfer'},
-            inputs: {type: 'textarea', label: 'Inputs', defaultValue: '[{"type": "address", "value": "0xd82049204D8514c637f150C7231BFefC5C4937Ec"},{"type": "uint256", "value": "1000000000000000"}]'},
+            functionName: {type: 'input', label: 'Function Name', defaultValue: 'approve'},
+            inputs: {type: 'textarea', label: 'Inputs', defaultValue: '[{"type": "address", "value": "0xd82049204D8514c637f150C7231BFefC5C4937Ec"},{"type": "uint256", "value": "0"}]'},
             name: {type: 'input', label: 'Network name', placeholder: 'e.g. Rinkeby', network: true},
             nodeUrl: {type: 'input', label: 'Network node URL', placeholder: 'e.g. https://rinkeby.infura.io', network: true}
         });
@@ -218,8 +218,8 @@
             walletId: {type: 'wallet-select', label: 'From'},
             to: {type: 'input', label: 'Contract Address', defaultValue: 'TFynD51aEXaYzkAiNoX2GtGEZ4ESZn7P6e'},
             value: {type: 'input', label: 'Amount (in WEI)', defaultValue: '0'},
-            functionName: {type: 'input', label: 'Function Name', defaultValue: 'transfer'},
-            inputs: {type: 'textarea', label: 'Inputs', defaultValue: '[{"type": "address", "value": "TA311N5Thw4vAjjBLNNtqEZp3qVRpeKgHB"},{"type": "uint256", "value": "1"}]'}
+            functionName: {type: 'input', label: 'Function Name', defaultValue: 'approve'},
+            inputs: {type: 'textarea', label: 'Inputs', defaultValue: '[{"type": "address", "value": "TA311N5Thw4vAjjBLNNtqEZp3qVRpeKgHB"},{"type": "uint256", "value": "0"}]'}
         });
     };
 
@@ -261,8 +261,8 @@
             walletId: {type: 'wallet-select', label: 'From'},
             to: {type: 'input', label: 'Contract Address', defaultValue: '0x0000000000000000000000000000456E65726779'},
             amount: {type: 'input', label: 'Amount (GWEI)', defaultValue: '0'},
-            functionName: {type: 'input', label: 'Function Name', defaultValue: 'transfer'},
-            inputs: {type: 'textarea', label: 'Inputs', defaultValue: '[{"type": "address", "value": "0xd82049204D8514c637f150C7231BFefC5C4937Ec"},{"type": "uint256", "value": "1000000000000"}]'}
+            functionName: {type: 'input', label: 'Function Name', defaultValue: 'approve'},
+            inputs: {type: 'textarea', label: 'Inputs', defaultValue: '[{"type": "address", "value": "0xd82049204D8514c637f150C7231BFefC5C4937Ec"},{"type": "uint256", "value": "0"}]'}
         });
     };
 
