f2c2ccdd00036648cbea9ee2579dc2835913eed8
DavyVanRoy <davy.van.roy@venly.io>
2023-05-24 15:11:07 +0200

    feat(VENLY-5045): set environment to sandbox if url contains sandbox

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -5,6 +5,8 @@
 
     if (window.location.href.indexOf("demo-staging") > -1) {
         app.env = 'staging';
+    } else if (window.location.href.indexOf("demo-sandbox") > -1) {
+        app.env = 'sandbox';
     } else if (window.location.href.indexOf("demo-qa") > -1) {
         app.env = 'qa';
     } else if (window.location.href.indexOf("demo-qa2") > -1) {
@@ -24,7 +26,7 @@
         env: app.env,
         connect: app.env + (app.isLocal ? '-local' : ''),
         api: app.env === 'local' ? 'http://localhost:8581/api' : 'https://api-wallet-' + resolvedEnv + '.venly.io',
-        login: 'https://login' + resolvedEnv + '.arkane.network',
+        login: 'https://login' + resolvedEnv + '.venly.io',
         arketypeClientSecret: '02053a9d-8293-43c4-a201-f8669f1329af', // Only visible for demo purpose, this secret should be configured in application backend
     };
 
