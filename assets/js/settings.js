a3d5fd44202e23a9af4d3b77f8691c8c5a04b669
NicoPeeters <nico.peeters@venly.io>
2024-01-11 14:12:01 +0100

    task(): Simplify running Keycloak locally

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -18,15 +18,15 @@
     }
 
     app.clientId = 'Arketype';
-    app.isLocal = false;
+    app.useLocalConnect = false;
+    app.useLocalKeycloak = false;
 
     let resolvedEnv = resolveEnv(app.env);
 
     app.environment = {
         env: app.env,
-        connect: app.env + (app.isLocal ? '-local' : ''),
+        connect: app.env + (app.useLocalConnect ? '-local' : '') + (app.useLocalKeycloak ? '-local' : ''),
         api: app.env === 'local' ? 'http://localhost:8581/api' : 'https://api-wallet-' + resolvedEnv + '.venly.io',
-        login: 'https://login' + resolvedEnv + '.venly.io',
         arketypeClientSecret: '02053a9d-8293-43c4-a201-f8669f1329af', // Only visible for demo purpose, this secret should be configured in application backend
     };
 
