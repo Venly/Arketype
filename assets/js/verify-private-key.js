dedc8bd007356e67bce7d441349a4928eeb3024a
NicoPeeters <nico.peeters@venly.io>
2024-04-17 15:39:50 +0200

    task() - Add verify private key page

--- assets/js/verify-private-key.js
+++ assets/js/verify-private-key.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     const btnVerify = document.getElementById("btnVerify");
+    const inptPrivateKey = document.getElementById("private-key")
 
     function getWalletAddress(pk) {
         try {
@@ -8,17 +9,22 @@ window.onload = function() {
             console.log(e);
             return "invalid";
         }
-
     }
 
-    btnVerify.addEventListener("click", function() {
-
-        let privateKey = document.getElementById("private-key").value;
+    const verifyPrivateKey = function() {
+        let privateKey = inptPrivateKey.value;
         document.getElementById("zeroX").innerHTML = privateKey.startsWith('0x');
         if (privateKey.startsWith("0x")) privateKey = privateKey.slice(2);
         document.getElementById("pkLength").innerHTML = privateKey.length;
         document.getElementById("pkLength").innerHTML = privateKey.length;
         document.getElementById("hexCheck").innerHTML = ethers.utils.isHexString(`0x${privateKey}`, 32);
         document.getElementById("walletAddress").innerHTML = getWalletAddress(privateKey);
-    });
+    };
+
+    btnVerify.addEventListener("click", verifyPrivateKey);
+    inptPrivateKey.addEventListener("keyup", function(e) {
+        if (e.key === 'Enter') {
+            verifyPrivateKey();
+        }
+    })
 };
